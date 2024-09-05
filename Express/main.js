07dd78523d7c6746901608b4e7c8199da222cba4
Naveen0823 <naveennaveen081426@gmail.com>
2024-09-05 16:20:57 +0530

    task commit

--- Express/main.js
+++ Express/main.js
@@ -4,9 +4,7 @@ app.use(express.json());
 app.get("/myname",(req,res)=>{
     res.json({"msg":"Naveen"});
 });
-app.listen(8080,()=>{
-    console.log("server started")
-})
+
 
 app.post("/myname",(req,res)=>{
     res.json({"msg":"Naveen KM"});
@@ -33,6 +31,66 @@ if(!name || !email || !password || !address){
 }else{
     res.json({msg:"registerd successfully"})
 }  
+});
+
+var {MongoClient} = require('mongodb');
+
+const Database_Name ="office"
+const MONGO_url = 'mongodb_url';
+const client = new MongoClient('mongodb+srv://naveennaveen081426:NAVEEN2002@cluster0.e8z0f.mongodb.net/');
+
+app.post("/createEmployee",async(req,res)=>{
+    let {name,email,password,mobile_no} = req.body;
+    let data = {
+        'name': 'Naveen',
+        'email': 'Naveen@gmail',
+        'password':'Text',
+        'mobile_no':'987645466'
+
+    }
+    await client.connect();
+    let db = client.db(Database_Name);
+     await db.collection("employee").insertOne(data);
+     res.status(200).json({"message":"employee created!!"})
+});
+
+
+
+app.get("/createEmployee",async(req,res)=>{
+    await client.connect();
+    let db = client.db("Office");
+    let list = await db.collection("employee").find({}).toArray();
+    res.status(200).json(list)
+});
+
+
+
+
+app.get("/listempbyname/:name",async(req,res)=>{
+    await client.connect();
+    let {name} = req.params;
+    let db = client.db("office");
+    let list = await db.collection("employee").find({"name":name}).toArray();
+    res.status(200).json(list)
+});
+
+
+app.post("/loginn",async(req,res)=>{
+    await client.connect();
+    let {name,password} = req.body;
+    let db = client.db("office");
+    let list = await db.collection("employee").find({"name":name,"password":password}).toArray();
+    if(list.length>0){
+        res.json({"msg":"you are correct"})
+    }else{
+        res.status(400).json({"msg":"you are wrong"})
+
+    }
+    
+});
+
+app.listen(8080,()=>{
+    console.log("server started")
 });
 
 
